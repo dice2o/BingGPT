f4265cc2bf06f9380eedfd5c0f34e48aa64c3616
cnjimbo <happycoder@foxmail.com>
2023-05-05 14:57:47 +0800

    add debug config for windows

--- main.js
+++ main.js
@@ -32,6 +32,10 @@ const configSchema = {
     type: 'string',
     default: '',
   },
+  isDevelopment: {
+    type: 'boolean',
+    default: true,
+  },
 }
 const config = new Store({ schema: configSchema, clearInvalidConfig: true })
 
@@ -44,6 +48,7 @@ const createWindow = () => {
       : theme === 'dark'
         ? true
         : false
+  const isDevelopment = config.get('isDevelopment', false)
   // Create window
   const mainWindow = new BrowserWindow({
     title: 'BingGPT',
@@ -51,7 +56,7 @@ const createWindow = () => {
     icon: 'icon.png',
     width: 601,
     height: 800,
-    titleBarStyle: 'hidden',
+    titleBarStyle: isDevelopment ? 'default' : 'hidden',
     titleBarOverlay: true,
     titleBarOverlay: {
       color: isDarkMode ? '#3b3b3b' : '#ffffff',
@@ -59,7 +64,7 @@ const createWindow = () => {
     },
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
-      devTools: false,
+      devTools: isDevelopment,
       nodeIntegration: true,
     },
   })
@@ -68,8 +73,9 @@ const createWindow = () => {
   mainWindow.setAlwaysOnTop(alwaysOnTop)
   // Get language
   const locale = app.getLocale() || 'en-US'
-  // Hide main menu (Windows)
-  Menu.setApplicationMenu(null)
+  // Hide main menu (Windows) 
+  if (!isDevelopment) //. Duo to the electron's feature, can't set menu dynamicly
+    Menu.setApplicationMenu(null)
   // Set proxy
   const proxy = config.get('proxy')
   if (proxy) {
@@ -250,12 +256,23 @@ const createWindow = () => {
           shell.openExternal('https://github.com/dice2o/BingGPT/releases')
         },
       },
+      {
+        type: 'separator',
+        visible: parameters.selectionText.trim().length === 0,
+      },
+      {
+        label: 'Development',
+        type: 'checkbox',
+        checked: isDevelopment,
+        visible: parameters.selectionText.trim().length === 0,
+        click: (menuItem, browserWindow, event) => isDevelopmentHandler(menuItem, browserWindow, event),
+      },
     ],
   })
   // Load Bing
   const bingUrl = `https://edgeservices.bing.com/edgediscover/query?&${
     isDarkMode ? 'dark' : 'light'
-  }schemeovr=1&FORM=SHORUN&udscs=1&udsnav=1&setlang=${locale}&features=udssydinternal&clientscopes=windowheader,coauthor,chat,&udsframed=1`
+    }schemeovr=1&FORM=SHORUN&udscs=1&udsnav=1&setlang=${locale}&features=udssydinternal&clientscopes=windowheader,coauthor,chat,&udsframed=1`
   const userAgent =
     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36 Edg/114.0.0.0'
   mainWindow.loadURL(bingUrl)
@@ -310,6 +327,26 @@ const createWindow = () => {
   const alwaysOnTopHandler = () => {
     config.set('alwaysOnTop', !mainWindow.isAlwaysOnTop())
     mainWindow.setAlwaysOnTop(!mainWindow.isAlwaysOnTop())
+  }
+  // is Development Handler
+  const isDevelopmentHandler = (menuItem, browserWindow, event) => {
+
+    dialog
+      .showMessageBox(browserWindow, {
+        type: 'question',
+        buttons: ['Yes', 'No'],
+        message: 'isDevelopment Saved',
+        detail: 'Do you want to reload BingGPT now?',
+      })
+      .then((result) => {
+        if (result.response === 0) {
+          config.set('isDevelopment', !isDevelopment)
+        }
+        return result
+      }).then(l => {
+        browserWindow.close()
+        createWindow()
+      })
   }
   // Theme
   const themeHandler = (newTheme) => {
